0e4dcd53735a992ec24c0ee54dafc9b4efddd786
谢翰章； <xiehanzhang@hrfax.cn>
2023-01-18 15:26:44 +0800

    feat:'first'

--- src/layout/Sider/index.tsx
+++ src/layout/Sider/index.tsx
@@ -1,53 +1,77 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Menu } from 'antd';
 import {
-    MenuUnfoldOutlined,
-    MenuFoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-  } from '@ant-design/icons';
-import { useNavigate } from "react-router-dom";
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { useNavigate, useLocation } from "react-router-dom";
 import style from './index.module.scss'
 const Sider: React.FC = () => {
-const { Sider } = Layout;
-const navigate = useNavigate()
-const [collapsed, setCollapsed] = useState<boolean>(false);
-const onchanges = (path:any)=>{
+  const { Sider } = Layout;
+  const navigate = useNavigate()
+  const currentRoute = useLocation()
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  // const [openKeys,setOpenKeys] = useState("")
+  const menuList = [
+    {
+      key: '/home',
+      icon: <UserOutlined />,
+      label: '首页',
+    },
+    {
+      key: '/home/menu',
+      icon: <VideoCameraOutlined />,
+      label: '菜单',
+    },
+    {
+      key: '1',
+      icon: <UploadOutlined />,
+      label: 'demo测试',             
+      children: [
+        {
+          key: "/home/demo",
+          label: '个人信息',
+        }
+      ]
+    },
+  ]
+  // useEffect(()=>{
+  //   findOpen()
+  // },[])
+  // const findOpen=()=>{
+  //   menuList.map(item=>{
+  //     if(item.children && item.children.length>0){
+  //       if(item.children.find(v=>v.key === currentRoute.pathname)){
+  //         console.log(item,"有相等的",item.key)
+  //         setOpenKeys(item.key)
+  //       }
+  //     }
+  //   })
+  // }
+  const onchanges = (path: any) => {
     navigate(path.key)
-}
-return (
+  }
+  return (
     <Sider trigger={null} collapsible collapsed={collapsed} className="page">
       <div className={style['icon-box']}>
-      {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: style['collapsed-icon'],
-            onClick: () => setCollapsed(!collapsed),
-          })}
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+          className: style['collapsed-icon'],
+          onClick: () => setCollapsed(!collapsed),
+        })}
       </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['/']}
-          onSelect={(item)=>onchanges(item)}
-          items={[
-            {
-              key: '/',
-              icon: <UserOutlined />,
-              label: '首页',
-            },
-            {
-              key: '/home/menu',
-              icon: <VideoCameraOutlined />,
-              label: '菜单',
-            },
-            {
-              key: '/home/demo',
-              icon: <UploadOutlined />,
-              label: 'demo',
-            },
-          ]}
-        />
-      </Sider>
-)
+      {/* defaultOpenKeys={[openKeys]}
+        openKeys={[openKeys]} */}
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={[currentRoute.pathname]}
+        onSelect={(item) => onchanges(item)}
+        items={menuList}
+      />
+    </Sider>
+  )
 };
 export default Sider
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,13 +2,19 @@ import style from './index.module.scss'
 import { connect } from 'react-redux'
 import { increment, decrement } from '@/redux/action'
 import { Button } from 'antd'
+import { useState } from 'react'
 const Home = (props: any) => {
+    const [num,setNum] = useState<number>(1)
     const add = () => {
         console.log(props)
         props.increment(1)
     };
     const reduce = () => {
         props.decrement(1)
+    }
+    const changeVal =()=>{
+        setNum(num+1)
+        console.log("改变值")
     }
     return (
         <>
@@ -18,6 +24,8 @@ const Home = (props: any) => {
             {props.count}
             <Button type='primary' onClick={(() => add())}>加</Button>
             <Button type='primary' onClick={(() => reduce())}>减</Button>
+            {num}
+            <Button onClick={()=>changeVal()}>加一</Button>
         </>
     )
 }
